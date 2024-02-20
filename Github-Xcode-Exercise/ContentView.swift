1485a3848a16dde73a762cef9da82cb8f8057646
devingrischow <devingrischow@outlook.com>
2024-02-20 13:02:49 -0600

    Initial Commit

---
+++ Github-Xcode-Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github-Xcode-Exercise
+//
+//  Created by Devin Grischow on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github-Xcode-Exercise/Github_Xcode_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Xcode_ExerciseApp.swift
+//  Github-Xcode-Exercise
+//
+//  Created by Devin Grischow on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_Xcode_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
