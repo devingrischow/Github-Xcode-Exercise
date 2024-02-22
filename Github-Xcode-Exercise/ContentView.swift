e8be420a05f0e30e2eb1d62579a878207745540c
devingrischow <devingrischow@outlook.com>
2024-02-22 16:32:36 -0600

    Tab Bar Combination is ready. Order Goes Merge Tab Bar, Then Merge Elevate, Then Slate

--- Github-Xcode-Exercise/ContentView.swift
+++ Github-Xcode-Exercise/ContentView.swift
@@ -7,18 +7,39 @@
 
 import SwiftUI
 
+let tabBarItems:[TabChoices] = [.view1, .view2]
+
 struct ContentView: View {
+    
+    //View Options For Tab Bar
+
+    //State For Selected Tab, Starts at
+    @State var selectedTab:TabChoices = .view1
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader{ geometry in
+            TabView(selection: $selectedTab){
+                TeamElevate().tag(TabChoices.view1)
+                //SampleView2().tag(TabChoices.view2)
+                TeamSlate().tag(TabChoices.view2)
+            }//Bottom of Tab View
+            .overlay(alignment: .bottom){
+                TeamChoicesTabBar(selectedTab: $selectedTab, viewWidth: geometry.size.width - 30)//To get the effect of the tab bar being smaller than the phone, take the screen width (393 on iphone 15 pro) and subtract 30 from the size, this it to allow for easier resizing on any phone
+            }
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+enum TabChoices:String, Identifiable{
+    case view1 = "line.diagonal.arrow"
+    case view2 = "line.diagonal"
+    
+    var id: Self { return self }
 }
